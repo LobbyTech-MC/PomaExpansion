d45833fe84891b0bf594c0a4f38177fb9d4b192a
poma123 <25465545+poma123@users.noreply.github.com>
2020-12-25 20:57:38 +0100

    Updated dependencies

--- src/main/java/me/poma123/pomaexpansion/AdvancedMassFabricator.java
+++ src/main/java/me/poma123/pomaexpansion/AdvancedMassFabricator.java
@@ -1,7 +1,7 @@
 package me.poma123.pomaexpansion;
 
+import dev.j3fftw.extrautils.utils.Utils;
 import dev.j3fftw.litexpansion.Items;
-import dev.j3fftw.litexpansion.utils.Utils;
 import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetComponent;
 import io.github.thebusybiscuit.slimefun4.core.networks.energy.EnergyNetComponentType;
 import io.github.thebusybiscuit.slimefun4.utils.ChestMenuUtils;
@@ -25,15 +25,13 @@ import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Map;
 
-import static io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems.ADVANCED_CIRCUIT_BOARD;
-import static io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems.REINFORCED_PLATE;
-
 public class AdvancedMassFabricator extends SlimefunItem implements InventoryBlock, EnergyNetComponent {
 
         public static final RecipeType RECIPE_TYPE = new RecipeType(
                 new NamespacedKey(PomaExpansion.getInstance(), "advanced_mass_fabricator"), PomaExpansion.ADVANCED_MASS_FABRICATOR_MACHINE
         );
 
+        private static final int[] BORDER = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26};
         public static final int ENERGY_CONSUMPTION = 200;
         public static final int CAPACITY = 1024;
 
@@ -57,98 +55,112 @@ public class AdvancedMassFabricator extends SlimefunItem implements InventoryBlo
             PomaExpansion.getInstance().registerRecipe(Items.UU_MATTER, Items.SCRAP, AdvancedMassFabricator.RECIPE_TYPE);
         }
 
-        private void setupInv() {
-            createPreset(this, "&4Advanced Mass Fabricator", blockMenuPreset -> {
-                for (int i = 0; i < 27; i++)
-                    blockMenuPreset.addItem(i, ChestMenuUtils.getBackground(), ChestMenuUtils.getEmptyClickHandler());
-
-                for (int slot : INPUT_SLOTS)
-                    blockMenuPreset.addItem(slot, null, (player, i, itemStack, clickAction) -> true);
-
-                Utils.putOutputSlot(blockMenuPreset, OUTPUT_SLOT);
-
-                blockMenuPreset.addItem(PROGRESS_SLOT, progressItem);
-            });
-        }
-
-        @Override
-        public void preRegister() {
-            this.addItemHandler(new BlockTicker() {
-                public void tick(Block b, SlimefunItem sf, Config data) {
-                    AdvancedMassFabricator.this.tick(b);
-                }
-
-                public boolean isSynchronized() {
-                    return false;
-                }
-            });
-        }
-
-        private void tick(@Nonnull Block b) {
-            @Nullable final BlockMenu inv = BlockStorage.getInventory(b);
-            if (inv == null) return;
-
-            // yes this is ugly shush
-            @Nullable ItemStack input = inv.getItemInSlot(INPUT_SLOTS[0]);
-            @Nullable ItemStack input2 = inv.getItemInSlot(INPUT_SLOTS[1]);
-            @Nullable final ItemStack output = inv.getItemInSlot(OUTPUT_SLOT);
-            if (output != null && output.getAmount() == output.getMaxStackSize()) return;
-
-            if (!SlimefunUtils.isItemSimilar(input, Items.SCRAP, true))
-                input = null;
-            if (!SlimefunUtils.isItemSimilar(input2, Items.SCRAP, true))
-                input2 = null;
-
-            if (input == null && input2 == null) return;
-
-            final BlockPosition pos = new BlockPosition(b.getWorld(), b.getX(), b.getY(), b.getZ());
-            int currentProgress = progress.getOrDefault(pos, 0);
-
-            if (!takePower(b.getLocation())) return;
-
-            // Process first tick - remove an input and put it in map.
-            if (currentProgress != PROGRESS_AMOUNT) {
-                if (input != null)
-                    inv.consumeItem(INPUT_SLOTS[0]);
-                else
-                    inv.consumeItem(INPUT_SLOTS[1]);
-                progress.put(pos, ++currentProgress);
-                ChestMenuUtils.updateProgressbar(inv, PROGRESS_SLOT, PROGRESS_AMOUNT - currentProgress,
-                        PROGRESS_AMOUNT, progressItem);
-            } else {
-                if (output != null && output.getAmount() > 0)
-                    output.setAmount(output.getAmount() + 1);
-                else
-                    inv.replaceExistingItem(OUTPUT_SLOT, Items.UU_MATTER.clone());
-                progress.remove(pos);
-                ChestMenuUtils.updateProgressbar(inv, PROGRESS_SLOT, PROGRESS_AMOUNT, PROGRESS_AMOUNT, progressItem);
+    private void setupInv() {
+        createPreset(this, "&4Advanced Mass Fabricator", blockMenuPreset -> {
+            for (int i : BORDER) {
+                blockMenuPreset.addItem(i, ChestMenuUtils.getBackground(), ChestMenuUtils.getEmptyClickHandler());
             }
+
+            Utils.putOutputSlot(blockMenuPreset, OUTPUT_SLOT);
+
+            blockMenuPreset.addItem(PROGRESS_SLOT, progressItem);
+            blockMenuPreset.addMenuClickHandler(PROGRESS_SLOT, ChestMenuUtils.getEmptyClickHandler());
+
+        });
+    }
+
+    @Override
+    public void preRegister() {
+        this.addItemHandler(new BlockTicker() {
+            public void tick(Block b, SlimefunItem sf, Config data) {
+                AdvancedMassFabricator.this.tick(b);
+            }
+
+            public boolean isSynchronized() {
+                return false;
+            }
+        });
+    }
+
+    private void tick(@Nonnull Block b) {
+        @Nullable final BlockMenu inv = BlockStorage.getInventory(b);
+        if (inv == null) {
+            return;
         }
 
-        private boolean takePower(@Nonnull Location loc) {
-            if (getCharge(loc) < ENERGY_CONSUMPTION) return false;
-            removeCharge(loc, ENERGY_CONSUMPTION);
-            return true;
+        // yes this is ugly shush
+        @Nullable ItemStack input = inv.getItemInSlot(INPUT_SLOTS[0]);
+        @Nullable ItemStack input2 = inv.getItemInSlot(INPUT_SLOTS[1]);
+        @Nullable final ItemStack output = inv.getItemInSlot(OUTPUT_SLOT);
+        if (output != null && (output.getType() != Items.UU_MATTER.getType()
+                || output.getAmount() == output.getMaxStackSize()
+                || !Items.UU_MATTER.getItem().isItem(output))) {
+            return;
         }
 
-        @Override
-        public EnergyNetComponentType getEnergyComponentType() {
-            return EnergyNetComponentType.CONSUMER;
+        if (!Items.SCRAP.getItem().isItem(input)) {
+            input = null;
+        }
+        if (!Items.SCRAP.getItem().isItem(input2)) {
+            input2 = null;
+        }
+        if (input == null && input2 == null) {
+            return;
         }
 
-        @Override
-        public int getCapacity() {
-            return CAPACITY;
+        final BlockPosition pos = new BlockPosition(b.getWorld(), b.getX(), b.getY(), b.getZ());
+        int currentProgress = progress.getOrDefault(pos, 0);
+
+        if (!takePower(b)) {
+            return;
         }
 
-        @Override
-        public int[] getInputSlots() {
-            return INPUT_SLOTS;
+        // Process first tick - remove an input and put it in map.
+        if (currentProgress != PROGRESS_AMOUNT) {
+            if (input != null)
+                inv.consumeItem(INPUT_SLOTS[0]);
+            else
+                inv.consumeItem(INPUT_SLOTS[1]);
+            progress.put(pos, ++currentProgress);
+            ChestMenuUtils.updateProgressbar(inv, PROGRESS_SLOT, PROGRESS_AMOUNT - currentProgress,
+                    PROGRESS_AMOUNT, progressItem);
+        } else {
+            if (output != null && output.getAmount() > 0) {
+                output.setAmount(output.getAmount() + 1);
+            } else {
+                inv.replaceExistingItem(OUTPUT_SLOT, Items.UU_MATTER.clone());
+            }
+            progress.remove(pos);
+            ChestMenuUtils.updateProgressbar(inv, PROGRESS_SLOT, PROGRESS_AMOUNT, PROGRESS_AMOUNT, progressItem);
         }
+    }
 
-        @Override
-        public int[] getOutputSlots() {
-            return new int[] {OUTPUT_SLOT};
+    private boolean takePower(@Nonnull Block b) {
+        if (getCharge(b.getLocation()) < ENERGY_CONSUMPTION) {
+            return false;
         }
+        removeCharge(b.getLocation(), ENERGY_CONSUMPTION);
+        return true;
+    }
 
+    @Nonnull
+    @Override
+    public EnergyNetComponentType getEnergyComponentType() {
+        return EnergyNetComponentType.CONSUMER;
+    }
+
+    @Override
+    public int getCapacity() {
+        return CAPACITY;
+    }
+
+    @Override
+    public int[] getInputSlots() {
+        return INPUT_SLOTS;
+    }
+
+    @Override
+    public int[] getOutputSlots() {
+        return new int[] {OUTPUT_SLOT};
+    }
 }
